84fea99a817dcbb6f2ce7978c641dc1d5d453446
Yusuke Yamada <yamachu.dev@gmail.com>
2017-09-04 09:38:39 +0900

    User の追加から DB の config 取得までの切り出し

---
+++ RealmSyncClinetLibrary/APIClient/APIServerHTTPClient.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using RealmNetCoreSample.Services;
+
+namespace RealmSyncClinetLibrary.APIClient
+{
+    public class APIServerHttpClient
+    {
+        private HttpClient HttpClient;
+        private static PasswordHashService PasswordService = new PasswordHashService();
+
+        public APIServerHttpClient(string accessToken)
+        {
+            HttpClient = new HttpClient(new APIServerHttpClientHandler(accessToken))
+            {
+                BaseAddress = new Uri("http://localhost:5001"),
+            };
+        }
+
+        public async Task<string> GetDatabaseConfig()
+        {
+            var response = await HttpClient.GetAsync("/api/database/config").ConfigureAwait(false);
+            var responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            var json = JsonConvert.DeserializeObject<DatabaseConfigModel>(responseBody);
+
+            return json.Server;
+        }
+
+        public static string GenerateAccessTokenFromNamePass(string userName, string password)
+        {
+            var hashedPass = PasswordService.GetHashedString(password);
+
+            return PasswordService.GenerateAccessToken(userName, hashedPass);
+        }
+
+        public static async Task<bool> PostUserCreate(string userName, string password)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:5001")
+            };
+
+            var contentBody = new
+            {
+                name = userName,
+                password = password,
+            };
+
+            var content = JsonConvert.SerializeObject(contentBody);
+
+            var response = await client.PostAsync("/api/user/create",
+                                                  new StringContent(content, Encoding.UTF8, "application/json"))
+                                       .ConfigureAwait(false);
+
+            var created = (int)response.StatusCode;
+
+            return created == 201;
+        }
+
+        internal class DatabaseConfigModel
+        {
+            public string Server { get; set; }
+        }
+    }
+}
---
+++ RealmSyncClinetLibrary/APIClient/APIServerHttpClientHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RealmSyncClinetLibrary.APIClient
+{
+    public class APIServerHttpClientHandler : DelegatingHandler
+    {
+        private readonly string AccessToken;
+
+        public APIServerHttpClientHandler(string accessToken) : this(new HttpClientHandler(), accessToken)
+        {
+        }
+
+        public APIServerHttpClientHandler(HttpClientHandler handler, string accessToken) : base(handler)
+        {
+            AccessToken = accessToken;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("X-AuthToken", AccessToken);
+            request.Headers.Add("Accept", "application/json");
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
