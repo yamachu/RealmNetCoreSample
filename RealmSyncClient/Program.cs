a82b81cfcae442a7e107101c06a0751199bf7fe0
Yusuke Yamada <yamachu.dev@gmail.com>
2017-09-04 09:40:16 +0900

    DB コンフィグ取得までの実装

--- RealmSyncClient/Program.cs
+++ RealmSyncClient/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Threading.Tasks;
+using RealmSyncClinetLibrary.APIClient;
 
 namespace RealmSyncClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // すでに作ってあるなら不要
+            var created = await APIServerHttpClient.PostUserCreate("hoge@hoge", "fuga");
+            if (!created)
+            {
+               Console.WriteLine("Create Post failed");
+               return;
+            }
+
+            var accessToken = APIServerHttpClient.GenerateAccessTokenFromNamePass("hoge@hoge", "fuga");
+            var client = new APIServerHttpClient(accessToken);
+
+            var dbUrl = await client.GetDatabaseConfig();
+
+            Console.WriteLine(dbUrl);
         }
     }
 }
