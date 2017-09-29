0a8ac3f026add21d07c136318ec386ec9c79861b
Yusuke Yamada <yamachu.dev@gmail.com>
2017-09-29 20:58:52 +0900

    Xamarin.Forms shared のプロジェクトを追加
    Androidが手元でビルドできなかったので一旦コメントアウト

---
+++ RealmXamSample/RealmXamSamplePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using RealmSyncClinetLibrary.APIClient;
+using RealmSyncClinetLibrary.SyncLib;
+
+namespace RealmXamSample
+{
+    public partial class RealmXamSamplePage : ContentPage
+    {
+        public RealmXamSamplePage()
+        {
+            InitializeComponent();
+            Setup();
+        }
+
+        async private void Setup()
+        {
+            var accessToken = APIServerHttpClient.GenerateAccessTokenFromNamePass("hoge@hoge", "fuga");
+            var client = new APIServerHttpClient(accessToken);
+
+            var dbUrl = await client.GetDatabaseConfig();
+
+            var shared = await SharedModelSync.Initialize("hoge@hoge", "fuga", "localhost", 9080, dbUrl);
+
+            var announce = shared.GetAnnouncement();
+            this.BindingContext = announce;
+        }
+    }
+}
